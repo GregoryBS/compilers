bd909cc4fd0d49be993e116815151a31a8417a67
gregory <aanonimov117@gmail.com>
2022-05-02 17:58:07 +0300

    change main for modelling of fsm

--- lang_recogn/main.go
+++ lang_recogn/main.go
@@ -24,11 +24,17 @@ func main() {
 				fsm = analysis.BuildFSM(scanner.Text())
 				if fsm == nil {
 					fmt.Println("Error occured while building fsm")
+				} else {
+					fsm.GetGraph()
 				}
 			}
 		case "2":
 			if scanner.Scan() {
-				fmt.Println("word", scanner.Text())
+				if fsm != nil {
+					fmt.Println("word is acceptable:", analysis.Modelling(fsm, scanner.Text()))
+				} else {
+					fmt.Println("Error occured while modelling fsm")
+				}
 			}
 		case "0":
 			os.Exit(0)
