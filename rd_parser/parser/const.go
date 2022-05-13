dde1b78ac21031cbba68b7cce034f6f7c5ab872e
gregory <aanonimov117@gmail.com>
2022-05-13 18:58:28 +0300

    change parser funcs for creating graphviz tree

--- rd_parser/parser/const.go
+++ rd_parser/parser/const.go
@@ -2,6 +2,8 @@ package parser
 
 import (
 	"unicode"
+
+	"github.com/goccy/go-graphviz/cgraph"
 )
 
 const (
@@ -22,6 +24,7 @@ const (
 	LBRACE   = '{'
 	RBRACE   = '}'
 	SEP      = ';'
+	EPS      = "eps"
 )
 
 func (p *Parser) apply(term string) {
@@ -32,52 +35,68 @@ func (p *Parser) apply(term string) {
 	p.Index += len(term)
 }
 
-func (p *Parser) addSign() {
+func (p *Parser) addSign(begin *cgraph.Node) {
 	switch p.Data[p.Index] {
 	case PLUS, MINUS:
+		s := p.Graph.AddNode(string(p.Data[p.Index]))
 		p.apply("")
+		p.Graph.AddEdges(begin, s)
 	default:
 		panic("Add operation sign expected")
 	}
 }
 
-func (p *Parser) mulSign() {
+func (p *Parser) mulSign(begin *cgraph.Node) {
 	switch p.Data[p.Index] {
 	case MUL, DIV, MOD:
+		s := p.Graph.AddNode(string(p.Data[p.Index]))
 		p.apply("")
+		p.Graph.AddEdges(begin, s)
 	default:
 		panic("Multiply operation sign expected")
 	}
 }
 
-func (p *Parser) relationSign() {
+func (p *Parser) relationSign(begin *cgraph.Node) {
 	sign := p.Data[p.Index : p.Index+2]
 	if sign == LE || sign == GE || sign == NE {
+		s := p.Graph.AddNode(sign)
 		p.apply(sign)
+		p.Graph.AddEdges(begin, s)
 	} else if sign[0] == LT || sign[0] == GT || sign[0] == EQ {
+		s := p.Graph.AddNode(sign[:1])
 		p.apply("")
+		p.Graph.AddEdges(begin, s)
 	} else {
 		panic("Relation operation sign expected")
 	}
 }
 
-func (p *Parser) number() {
+func (p *Parser) number(begin *cgraph.Node) {
 	if unicode.IsDigit(rune(p.Data[p.Index])) {
+		id := string(p.Data[p.Index])
 		p.apply("")
 		for unicode.IsDigit(rune(p.Data[p.Index])) {
+			id += string(p.Data[p.Index])
 			p.apply("")
 		}
+		end := p.Graph.AddNode(id)
+		p.Graph.AddEdges(begin, end)
 	} else {
 		panic("Number expected")
 	}
 }
 
-func (p *Parser) identifier() {
+func (p *Parser) identifier(begin *cgraph.Node) {
 	if unicode.IsLetter(rune(p.Data[p.Index])) {
+		id := string(p.Data[p.Index])
 		p.apply("")
 		for unicode.IsLetter(rune(p.Data[p.Index])) {
+			id += string(p.Data[p.Index])
 			p.apply("")
 		}
+		end := p.Graph.AddNode(id)
+		p.Graph.AddEdges(begin, end)
 	} else {
 		panic("Symbol identifier expected")
 	}
